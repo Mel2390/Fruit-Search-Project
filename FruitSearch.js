9594f7f3c7305cda09edf0cc014a890acf303bbf
Melanny <ziony23@gmail.com>
2024-06-27 14:14:51 -0700

    3 files

---
+++ FruitSearch.js
@@ -0,0 +1,37 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+  return fruit.filter(fruit => fruit.toLowerCase().includes(str.toLowerCase()));
+}
+
+function searchHandler(e) {
+  const inputValue = e.target.value;
+  const results = search(inputValue);
+  showSuggestions(results, inputValue);
+}
+
+function showSuggestions(results, inputValue) {
+  suggestions.innerHTML = '';
+  if (inputValue === '') {
+    return;
+  }
+  
+  results.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    suggestions.appendChild(li);
+  });
+}
+
+function useSuggestion(e) {
+  if (e.target.tagName === 'LI') {
+    input.value = e.target.textContent;
+    suggestions.innerHTML = '';
+  }
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
